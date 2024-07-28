89b171a97d36a8023374418156f73c1a7066dfd8
Tran Vu Lam <lam261204@gmail.com>
2024-07-28 19:49:27 +0700

    add notes

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,4 +1,5 @@
 const Product = require("../../models/product.model");
+// [GET] /product
 module.exports.index = async (req, res) => {
     const products = await Product.find({
         status: "active",
