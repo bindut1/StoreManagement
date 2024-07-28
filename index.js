15edb5337ad44be90ffcc52946def507546e2981
Tran Vu Lam <lam261204@gmail.com>
2024-07-28 18:51:06 +0700

    add layout chung

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const app = express()
 const port = process.env.PORT;
+const systemConfig = require("./config/system");
 
 const route = require("./routes/client/index.route");
 const routeAdmin = require("./routes/admin/index.route");
@@ -11,6 +12,8 @@ database.connect();
 app.set('views', './views');
 app.set('view engine', 'pug');
 app.use(express.static("public"));
+
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
 
 route(app);
 routeAdmin(app);
