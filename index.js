28ce49482ce9697e3bc08386175ecbe09c8f50b1
Tran Vu Lam <lam261204@gmail.com>
2024-07-28 18:16:17 +0700

    add trang tong quan

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const port = process.env.PORT;
 
 const route = require("./routes/client/index.route");
+const routeAdmin = require("./routes/admin/index.route");
 const database = require("./config/database");
 database.connect();
 
@@ -12,6 +13,7 @@ app.set('view engine', 'pug');
 app.use(express.static("public"));
 
 route(app);
+routeAdmin(app);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
