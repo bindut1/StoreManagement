7b88a8614ceebb242cd2fdf047c2549eed1142b8
Tran Vu Lam <lam261204@gmail.com>
2024-08-06 14:55:44 +0700

    optimal filter by status

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,7 @@
 
 const Product = require("../../models/product.model");
 
+const filterStatusHelper = require("../../helpers/filterstatus");
 
 // [GET] /admin/products
 
@@ -9,31 +10,9 @@ module.exports.index = async (req, res) => {
     let find = {
         // deleted: false
     };
-    let filtersStatus = [
-        {
-            name: "Tat ca",
-            status: "",
-            class: ""
-        },
-        {
-            name: "Hoat dong",
-            status: "active",
-            class: ""
-        },
-        {
-            name: "Dung hoat dong",
-            status: "inactive",
-            class: ""
-        }
-    ];
-    if (req.query.status) {
-        const index = filtersStatus.findIndex(item => item.status == req.query.status);
-        filtersStatus[index].class = "active";
-    }
-    else {
-        const index = filtersStatus.findIndex(item => item.status == "");
-        filtersStatus[index].class = "active";
-    }
+    
+    const filterStatus = filterStatusHelper(req.query);
+    
     if (req.query.status) {
         find.status = req.query.status;
     }
@@ -52,7 +31,7 @@ module.exports.index = async (req, res) => {
     res.render("admin/pages/products/index", {
         pageTitle: "Danh sach san pham",
         products: products,
-        filtersStatus: filtersStatus,
+        filterStatus: filterStatus,
         keyword: keyword
     });
 }
---
+++ helpers/filterstatus.js
@@ -0,0 +1,28 @@
+module.exports = (query) => {
+    let filtersStatus = [
+        {
+            name: "Tat ca",
+            status: "",
+            class: ""
+        },
+        {
+            name: "Hoat dong",
+            status: "active",
+            class: ""
+        },
+        {
+            name: "Dung hoat dong",
+            status: "inactive",
+            class: ""
+        }
+    ];
+    if (query.status) {
+        const index = filtersStatus.findIndex(item => item.status == query.status);
+        filtersStatus[index].class = "active";
+    }
+    else {
+        const index = filtersStatus.findIndex(item => item.status == "");
+        filtersStatus[index].class = "active";
+    }
+    return filtersStatus;
+}
