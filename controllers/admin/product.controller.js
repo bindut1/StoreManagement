25ab9ff8abb30e71d1026b92b9c97734cdb11227
Tran Vu Lam <lam261204@gmail.com>
2024-08-06 15:33:33 +0700

    optimal find

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -3,6 +3,8 @@ const Product = require("../../models/product.model");
 
 const filterStatusHelper = require("../../helpers/filterstatus");
 
+const searchHelper = require("../../helpers/search");
+
 // [GET] /admin/products
 
 module.exports.index = async (req, res) => {
@@ -17,11 +19,9 @@ module.exports.index = async (req, res) => {
         find.status = req.query.status;
     }
 
-    let keyword = "";
-    if (req.query.keyword) {
-        keyword = req.query.keyword;
-        const regex = new RegExp(keyword, "i");
-        find.title = regex;
+    const objSearch = searchHelper(req.query);
+    if(objSearch.regex){
+        find.title = objSearch.regex;
     }
 
     const products = await Product.find(find);
@@ -32,6 +32,6 @@ module.exports.index = async (req, res) => {
         pageTitle: "Danh sach san pham",
         products: products,
         filterStatus: filterStatus,
-        keyword: keyword
+        keyword: objSearch.keyword
     });
 }
