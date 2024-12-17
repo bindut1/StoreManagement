2083505db064312290c1edadc343ba250bda8702
Tran Vu Lam <lam261204@gmail.com>
2024-12-17 09:58:09 +0700

    add validate

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 const router = express.Router();
 const storageMulter = require("../../helpers/storageMulter");
 const upload = multer({ storage: storageMulter() });
-
+const validate = require("../../validates/admin/product.validate");
 const controller = require("../../controllers/admin/product.controller");
 
 router.get("/", controller.index);
@@ -13,6 +13,11 @@ router.patch("/change-status/:status/:id", controller.changeStatus);
 router.patch("/change-multi", controller.changeMulti);
 router.delete("/delete/:id", controller.deleteItem);
 router.get("/create", controller.create);
-router.post("/create", upload.single("thumbnail"), controller.createPost);
+router.post(
+  "/create",
+  upload.single("thumbnail"),
+  validate.createPost,
+  controller.createPost
+);
 
 module.exports = router;
