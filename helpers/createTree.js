7f010f186697d76b875ba3396fc3b5b640268bc1
Tran Vu Lam <lam261204@gmail.com>
2025-01-07 20:25:03 +0700

    fix index products

--- helpers/createTree.js
+++ helpers/createTree.js
@@ -1,8 +1,11 @@
+let count = 0;
 const createTreeModule = (arr, parentId = "") => {
   const tree = [];
   arr.forEach((item) => {
     if (item.parent_id === parentId) {
+      count++;
       const newItem = item;
+      newItem.index = count;
       const children = createTreeModule(arr, item.id);
       if (children.length > 0) newItem.children = children;
       tree.push(newItem);
@@ -12,6 +15,7 @@ const createTreeModule = (arr, parentId = "") => {
 };
 
 module.exports.createTree = (arr, parentId = "") => {
+  count = 0;
   const tree = createTreeModule(arr, (parentId = ""));
   return tree;
 };
