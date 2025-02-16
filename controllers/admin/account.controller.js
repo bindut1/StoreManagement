50b0e33fe4602339592f99fb82ee84c3f635dfe5
Tran Vu Lam <lam261204@gmail.com>
2025-02-16 17:29:01 +0700

    add login feature

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -1,6 +1,6 @@
 const Account = require("../../models/account.model");
-const systemConfig = require("../../config/system");
 const md5 = require("md5");
+const systemConfig = require("../../config/system");
 const Role = require("../../models/role.model");
 
 // [GET] /admin/accounts
