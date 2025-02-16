a6f99af0ba9f28e1e550205d2d25dd1d604c4678
Tran Vu Lam <lam261204@gmail.com>
2025-02-16 15:02:45 +0700

    modified account.index.pug

--- validates/admin/product-category.validate.js
+++ validates/admin/product-category.validate.js
@@ -1,6 +1,6 @@
 module.exports.createPost = (req, res, next) => {
   if (!req.body.title) {
-    req.flash("error", "Vui long nhap tieu de!");
+    req.flash("error", "Vui lòng nhập tiêu đề!");
     res.redirect("back");
     return;
   }
