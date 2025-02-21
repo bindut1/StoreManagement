c89dbebfa2a3fdd2f2ab3ea44a9d063dad9dd9ed
Tran Vu Lam <lam261204@gmail.com>
2025-02-21 10:44:24 +0700

    add personal infor

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -5,10 +5,10 @@ const router = express.Router();
 // const storageMulter = require("../../helpers/storageMulter");
 // const upload = multer({ storage: storageMulter() });
 const upload = multer();
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
 const validate = require("../../validates/admin/product.validate");
 const controller = require("../../controllers/admin/product.controller");
 
-const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
 
 router.get("/", controller.index);
 //su dung PATCH thay the cho GET de khi user truy cap link online khong the cap nhat duoc, boi vi khi dung GET user co the dung link online de cap nhat
