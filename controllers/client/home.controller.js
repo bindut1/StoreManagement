4b43819427f91a02aad0ead9db4f6eb6a590f281
Tran Vu Lam <lam261204@gmail.com>
2025-02-23 10:24:38 +0700

    add new products

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -8,8 +8,18 @@ module.exports.index = async (req, res) => {
     status: "active",
   }).limit(6);
   const newProductsFeatured = productsHelper.priceNewProducts(productsFeatured);
+
+  const productsNew = await Product.find({
+    deleted: false,
+    status: "active",
+  })
+    .sort({ position: "desc" })
+    .limit(6);
+  const newProductsNew = productsHelper.priceNewProducts(productsNew);
+
   res.render("client/pages/home/index.pug", {
     pageTitle: "Trang chủ",
     productsFeatured: newProductsFeatured,
+    productsNew: newProductsNew,
   });
 };
