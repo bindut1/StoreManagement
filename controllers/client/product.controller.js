903067950a8ff58ac18c5f2f5d9206bdc51a192d
Tran Vu Lam <lam261204@gmail.com>
2025-02-23 12:23:21 +0700

    add event for product-category on FE

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,18 +1,21 @@
-const Product = require("../../models/product.model.js")
-const productsHelper = require("../../helpers/product.js")
+const Product = require("../../models/product.model.js");
+const ProductCategory = require("../../models/product-category.model.js");
+
+const productsHelper = require("../../helpers/product.js");
+const productsCategorysHelper = require("../../helpers/products-category.js");
 
 // [GET] /products
 module.exports.index = async (req, res) => {
   const products = await Product.find({
     status: "active",
-    deleted: false
-  }).sort({ position: "desc" })
+    deleted: false,
+  }).sort({ position: "desc" });
   const newProducts = productsHelper.priceNewProducts(products);
   res.render("client/pages/products/index.pug", {
     pageTitle: "Danh sách sản phẩm",
-    products: newProducts
-  })
-}
+    products: newProducts,
+  });
+};
 
 // [GET] /products/:slug
 module.exports.detail = async (req, res) => {
@@ -33,4 +36,32 @@ module.exports.detail = async (req, res) => {
     req.flash("error", `Mã sản phẩm không tồn tại`);
     res.redirect(`/products`);
   }
+};
+
+// [GET] /products/:slugCategory
+module.exports.category = async (req, res) => {
+  const category = await ProductCategory.findOne({
+    slug: req.params.slugCategory,
+    status: "active",
+    deleted: false,
+  });
+  if (!category) {
+    return res.status(404).send("Category not found");
+  }
+  const listSubCategory = await productsCategorysHelper.getSubCategory(
+    category.id
+  );
+  const listSubCategoryId = listSubCategory.map((item) => item.id);
+
+  const products = await Product.find({
+    product_category_id: { $in: [category.id, ...listSubCategoryId] },
+    deleted: false,
+  }).sort({ position: "desc" });
+
+  const newProducts = productsHelper.priceNewProducts(products);
+
+  res.render("client/pages/products/index.pug", {
+    pageTitle: category.title,
+    products: newProducts,
+  });
 };
