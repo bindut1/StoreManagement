0a491edee90511b2e4c9935efebfc3f4e4ff08d9
Tran Vu Lam <lam261204@gmail.com>
2025-02-23 12:55:27 +0700

    modified detail product

--- helpers/product.js
+++ helpers/product.js
@@ -1,8 +1,20 @@
 // Tính giảm giá cho nhiều SP
 module.exports.priceNewProducts = (products) => {
-    const newProducts = products.map(item => {
-        item.priceNew = (item.price * (100 - item.discountPercentage) / 100).toFixed(0)
-        return item;
-    })
-    return newProducts;
-}
+  const newProducts = products.map((item) => {
+    item.priceNew = (
+      (item.price * (100 - item.discountPercentage)) /
+      100
+    ).toFixed(0);
+    return item;
+  });
+  return newProducts;
+};
+
+//Giảm giá 1 SP
+module.exports.priceNewProduct = (product) => {
+  const priceNew = (
+    (product.price * (100 - product.discountPercentage)) /
+    100
+  ).toFixed(0);
+  return priceNew;
+};
