337b4fb984bf38d019bcffa807b36127b7c18067
Tran Vu Lam <lam261204@gmail.com>
2025-02-25 23:07:58 +0700

    created cart and add products to cart

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,11 +1,18 @@
-const categoryMiddleware = require("../../middlewares/client/category.middleware");
+const categoryMiddleware = require("../../middlewares/client/category.middleware.js")
+const cartMiddleware = require("../../middlewares/client/cart.middleware.js")
 
-const productRoutes = require("./product.route");
-const homeRoutes = require("./home.route");
-const searchRoutes = require("./search.route");
+const homeRouters = require("./home.route.js")
+const productRouter = require("./product.route.js")
+const searchRouter = require("./search.route.js")
+const carthRouter = require("./cart.route.js")
 
 module.exports = (app) => {
-  app.use("/", categoryMiddleware.category, homeRoutes);
-  app.use("/products", categoryMiddleware.category, productRoutes);
-  app.use("/search", searchRoutes);
-};
+    app.use(categoryMiddleware.category)  
+    app.use(cartMiddleware.cardId)        
+
+    app.use("/", homeRouters);
+    app.use("/products", productRouter)
+    app.use("/products", productRouter)
+    app.use("/search", searchRouter)
+    app.use("/cart", carthRouter)
+}
