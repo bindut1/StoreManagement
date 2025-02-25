83b3b6e85912b5eab4a3718579facd597b218ed6
Tran Vu Lam <lam261204@gmail.com>
2025-02-25 21:15:46 +0700

    add search for client

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -2,8 +2,10 @@ const categoryMiddleware = require("../../middlewares/client/category.middleware
 
 const productRoutes = require("./product.route");
 const homeRoutes = require("./home.route");
+const searchRoutes = require("./search.route");
 
 module.exports = (app) => {
   app.use("/", categoryMiddleware.category, homeRoutes);
   app.use("/products", categoryMiddleware.category, productRoutes);
+  app.use("/search", searchRoutes);
 };
