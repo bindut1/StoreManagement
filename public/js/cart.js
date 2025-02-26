da71642c00e4ed20a8680e1063692e4e584e2d83
Tran Vu Lam <lam261204@gmail.com>
2025-02-26 20:58:48 +0700

    update quantity product in cart

---
+++ public/js/cart.js
@@ -0,0 +1,13 @@
+//cập nhật số lượng sp trong giỏ hàng
+const inputsQuantity = document.querySelectorAll("input[name='quantity']");
+if (inputsQuantity.length > 0) {
+  inputsQuantity.forEach((input) => {
+    input.addEventListener("change", () => {
+      const productId = input.getAttribute("product-id");
+      const quantity = input.value;
+      if (quantity > 0) {
+        window.location.href = `/cart/update/${productId}/${quantity}`;
+      }
+    });
+  });
+}
