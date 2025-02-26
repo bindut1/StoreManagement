892e71b6c20ed841dde7380fa3db29a711f378d2
Tran Vu Lam <lam261204@gmail.com>
2025-02-26 20:34:27 +0700

    delete product from cart

--- routes/client/cart.route.js
+++ routes/client/cart.route.js
@@ -5,5 +5,6 @@ const controller = require("../../controllers/client/cart.controller.js");
 
 router.get("/", controller.index);
 router.post("/add/:productId", controller.addPost);
+router.get("/delete/:productId", controller.delete);
 
 module.exports = router;
