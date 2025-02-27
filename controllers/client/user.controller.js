1ac3b3638d2fb7b84627afa387fa12347453a963
Tran Vu Lam <lam261204@gmail.com>
2025-02-27 13:28:55 +0700

    add login and logout for client

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -24,3 +24,43 @@ module.exports.registerPost = async (req, res) => {
   res.cookie("tokenUser", user.tokenUser);
   res.redirect("/");
 };
+
+// [GET] /login
+module.exports.login = async (req, res) => {
+  res.render("client/pages/user/login", {
+    pageTitle: "Đăng nhập",
+  });
+};
+
+// [POST] /login
+module.exports.loginPost = async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const user = await User.findOne({
+    email: email,
+    deleted: false,
+  });
+  if (!user) {
+    req.flash("error", "Email không tồn tại");
+    res.redirect("back");
+    return;
+  }
+  if (md5(password) != user.password) {
+    req.flash("error", "Mật khẩu không đúng");
+    res.redirect("back");
+    return;
+  }
+  if (user.status === "inactive") {
+    req.flash("error", "Tài khoản đang bị khóa");
+    res.redirect("back");
+    return;
+  }
+  res.cookie("tokenUser", user.tokenUser);
+  res.redirect("/");
+};
+
+// [GET] /logout
+module.exports.logout = async (req, res) => {
+  res.clearCookie("tokenUser");
+  res.redirect("back");
+};
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,5 +1,6 @@
 const categoryMiddleware = require("../../middlewares/client/category.middleware.js")
 const cartMiddleware = require("../../middlewares/client/cart.middleware.js")
+const userMiddleware = require("../../middlewares/client/user.middleware.js")
 
 const homeRouters = require("./home.route.js")
 const productRouter = require("./product.route.js")
@@ -11,6 +12,7 @@ const userRouter = require("./user.route.js")
 module.exports = (app) => {
     app.use(categoryMiddleware.category)  
     app.use(cartMiddleware.cardId)        
+    app.use(userMiddleware.infoUser)        
 
     app.use("/", homeRouters);
     app.use("/products", productRouter)
