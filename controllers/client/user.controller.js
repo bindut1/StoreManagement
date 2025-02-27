7a17b8a3d901ada0dc6c6ebae64eab908c1ea6d7
Tran Vu Lam <lam261204@gmail.com>
2025-02-27 21:58:36 +0700

    deleted uploads folder and add forgotPassword

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -1,14 +1,17 @@
 const md5 = require("md5");
 const User = require("../../models/user.model.js");
+const ForgotPassword = require("../../models/forgot-password.model.js");
+const generateHelper = require("../../helpers/generate.js");
+const sendMailHelper = require("../../helpers/sendMail.js");
 
-// [GET] /register
+// [GET] /user/register
 module.exports.register = async (req, res) => {
   res.render("client/pages/user/register", {
     pageTitle: "Đăng ký tài khoản",
   });
 };
 
-// [POST] /register
+// [POST] /user/register
 module.exports.registerPost = async (req, res) => {
   const existEmail = await User.findOne({
     email: req.body.email,
@@ -25,14 +28,14 @@ module.exports.registerPost = async (req, res) => {
   res.redirect("/");
 };
 
-// [GET] /login
+// [GET] /user/login
 module.exports.login = async (req, res) => {
   res.render("client/pages/user/login", {
     pageTitle: "Đăng nhập",
   });
 };
 
-// [POST] /login
+// [POST] /user/login
 module.exports.loginPost = async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
@@ -59,8 +62,92 @@ module.exports.loginPost = async (req, res) => {
   res.redirect("/");
 };
 
-// [GET] /logout
+// [GET] /user/logout
 module.exports.logout = async (req, res) => {
   res.clearCookie("tokenUser");
   res.redirect("back");
+};
+
+// [GET] /user/password/forgot
+module.exports.forgotPassword = async (req, res) => {
+  res.render("client/pages/user/forgot-password", {
+    pageTitle: "Lấy lại mật khẩu",
+  });
+};
+
+// [POST] /user/password/forgot
+module.exports.forgotPasswordPost = async (req, res) => {
+  const email = req.body.email;
+  const user = await User.findOne({
+    email: email,
+    deleted: false,
+  });
+  if (!user) {
+    req.flash("error", "Email không tồn tại");
+    res.redirect("back");
+    return;
+  }
+  const otp = generateHelper.generateRandomNumber(6);
+  const objForgotPassword = {
+    email: email,
+    otp: otp,
+    expireAt: Date.now(),
+  };
+  const forgotPassword = new ForgotPassword(objForgotPassword);
+  await forgotPassword.save();
+  const subject = "Mã OTP xác minh lấy lại mật khẩu";
+  const html = `Mã OTP: <b>${otp}</b>. Thời hạn sử dụng là 3 phút.`;
+  sendMailHelper.sendMail(email, subject, html);
+  res.redirect(`/user/password/otp?email=${email}`);
+};
+
+// [GET] /user/password/otp
+module.exports.otpPassword = async (req, res) => {
+  const email = req.query.email;
+  res.render("client/pages/user/otp-password", {
+    pageTitle: "Nhập mã OTP",
+    email: email,
+  });
+};
+
+// [POST] /user/password/otp
+module.exports.otpPasswordPost = async (req, res) => {
+  const email = req.body.email;
+  const otp = req.body.otp;
+  const result = await ForgotPassword.findOne({
+    email: email,
+    otp: otp,
+  });
+  if (!result) {
+    req.flash("error", "OTP không hợp lệ");
+    res.redirect("back");
+    return;
+  }
+  const user = await User.findOne({
+    email: email,
+  });
+  res.cookie("tokenUser", user.tokenUser);
+  res.redirect("/user/password/reset");
+};
+
+// [GET] /user/password/reset
+module.exports.resetPassword = async (req, res) => {
+  res.render("client/pages/user/reset-password", {
+    pageTitle: "Đổi mật khẩu",
+  });
+};
+
+// [POST] /user/password/reset
+module.exports.resetPasswordPost = async (req, res) => {
+  const password = req.body.password;
+  const tokenUser = req.cookies.tokenUser;
+  await User.updateOne(
+    {
+      tokenUser: tokenUser,
+    },
+    {
+      password: md5(password),
+    }
+  );
+  res.redirect("/");
 };
--- helpers/generate.js
+++ helpers/generate.js
@@ -7,3 +7,12 @@ module.exports.generateRandomString = (length) => {
   }
   return result;
 };
+
+module.exports.generateRandomNumber = (length) => {
+  const chars = "1234567890";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+};
